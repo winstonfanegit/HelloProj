7a6b5d310fddb5f6585ae0a076b6a213cc7a5a67
winstonfanegit <winstonfane@gmail.com>
2017-11-19 14:34:46 -0500

    Initial commit

---
+++ app/src/main/java/com/study/alex/helloproj/HelloActivity.java
@@ -0,0 +1,13 @@
+package com.study.alex.helloproj;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelloActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello);
+    }
+}
